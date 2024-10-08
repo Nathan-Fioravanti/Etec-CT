94226fdf2a210949accdf280c8658d544b3f7cfe
Nathan-Fioravanti <141074238+Nathan-Fioravanti@users.noreply.github.com>
2024-10-08 07:58:44 -0300

    Delete 2024/DS/Interface directory

--- 2024/DS/Interface/ContaBancaria.java
+++
@@ -1,6 +0,0 @@
-// Definindo a interface ContaBancaria
-public interface ContaBancaria {
-    void depositar(double valor); // Método para depósito
-    boolean sacar(double valor);  // Método para saque
-    double getSaldo();            // Método para obter o saldo
-}
