576a3350b1e9f2657b15c8c3c71fc87df897890d
Nathan-Fioravanti <141074238+Nathan-Fioravanti@users.noreply.github.com>
2024-10-09 08:06:46 -0300

    Atividade de tomada de decisão em javascript
    Atividade de tomada de decisão em javascript

---
+++ 2024/PAM/Aula 4(Atividade de decisão)/index.js
@@ -0,0 +1,22 @@
+
+var formas = prompt("digite um numero de lados de uma figura de até 10 lados ");
+
+    if (formas == 3){
+        console.log("O perímetro do triângulo é P=l+l+l e a área do mesmo é A= b⋅h/2");
+    } else if (formas == 4) {
+        console.log("O perímetro do quadrado é P=4a e a área do mesmo é A= a^2");
+    } else if (formas == 5){
+        console.log("O perímetro do pentágono é P=5a e a área do mesmo é A≈1.720⋅a^2");
+    } else if (formas == 6){
+        console.log("O perímetro do hexágono é P=6a e a área do mesmo é A=2.598⋅a^2");
+    } else if (formas == 7){
+        console.log("O perímetro do heptágono é P=7a e a área do mesmo é A≈3.634⋅a^2");
+    } else if (formas == 8){
+        console.log("O perímetro do octágono é P=8a e a área do mesmo é A=4.828⋅a^2");
+    } else if (formas == 9){
+        console.log("O perímetro do nonágono é P=9a e a área do mesmo é A≈6.881⋅a^2");
+    } else if (formas == 10){
+        console.log("O perímetro do decágono é P=10a e a área do mesmo é A≈7.694⋅a^2");
+    }
+
+
