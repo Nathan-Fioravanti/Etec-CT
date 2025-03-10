e4f8570adda820b4cc73fbb7b78f3e56f591dff9
Nathan-Fioravanti <141074238+Nathan-Fioravanti@users.noreply.github.com>
2025-03-10 11:43:33 -0300

    Calculadora automatizada com servidores diferentes (cors)

---
+++ 2025/Programação web/Calculadora_server/server/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+app.use(express.json());
+var cors = require('cors')
+
+
+
+app.use(cors({
+    origin: '*'
+}));
+
+app.get('/', function (req, res) {
+  const retorno = {
+      resp: "Hello World"
+  }
+  
+    res.send(retorno)
+}) 
+
+//---------------------------
+
+// Função para calcular a expressão no visor
+app.post('/calcular', (req, res) => {
+    try {
+        const resultado = eval(req.body.calculo);
+        res.json({resultado: resultado})
+    } catch(err) {
+       console.log(err);
+       res.status(500).json({error:err})
+    }
+})
+
+// Função para calcular a raiz quadrada do número exibido
+app.post('/calcularRaiz', (req, res) => {
+    const resultado = Math.sqrt(parseFloat(req.body.raiz));
+    res.json({resultado: resultado})
+})
+
+// Função para calcular o quadrado do número exibido
+app.post('/calcularQuadrado', (req, res) => {
+    const resultado = Math.pow(parseFloat(req.body.potenciacao), 2);
+    res.json({resultado: resultado})
+})
+
+app.listen(3000, () => console.log('Rodando na porta 3000'));
