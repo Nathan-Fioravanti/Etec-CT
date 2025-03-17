466566f5ee80e444b33cadd19c61ced9cf5f705b
Nathan-Fioravanti <141074238+Nathan-Fioravanti@users.noreply.github.com>
2025-03-17 11:46:39 -0300

    Calculadora vinculada com o servidor

---
+++ 2025/Programação web/calculadora_Server/front/js/script.js
@@ -0,0 +1,22 @@
+const url = 'http://localhost:3000/'
+
+async function getUrl(){
+    const resp = await fetch(url);
+    const retorno = await resp.json();
+    return retorno;
+}
+
+async function exibirUrl() {
+    const retorno = await getUrl();
+    console.log(retorno);
+}
+
+exibirUrl();
+
+// const url = "http://localhost:3000/"
+
+// const resp = await fetch(url);
+// const retorno = await resp.json();
+
+// console.log(retorno)
+
