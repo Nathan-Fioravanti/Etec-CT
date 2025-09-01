5db666b664e398aedc27d5424c30038bd754b3a4
Nathan-Fioravanti <141074238+Nathan-Fioravanti@users.noreply.github.com>
2025-09-01 08:03:33 -0300

    Update contaCorrente.test.js

--- 2025/Qualidade e teste de software/Teste de classe conta/test/contaCorrente.test.js
+++ 2025/Qualidade e teste de software/Teste de classe conta/test/contaCorrente.test.js
@@ -10,12 +10,13 @@ describe('ContaCorrente', () => {
  
   test('falha ao sacar quando saldo cobre apenas o valor, mas não a taxa', () => {
     expect(() => {
-      cc.sacar('001', '1234', 'senha123', 990); // 990 + 20 = 1010 > 1000
+      cc.sacar('001', '1234', 'senha123', 990); 
     }).toThrow('Saldo insuficiente para saque e taxa');
   });
  
   test('saca com sucesso e aplica taxa corretamente', () => {
-    cc.sacar('001', '1234', 'senha123', 200); // total = 220
+    cc.sacar('001', '1234', 'senha123', 200); 
     expect(cc.consultarSaldo('001', '1234', 'senha123')).toBe(780);
   });
+
 });
